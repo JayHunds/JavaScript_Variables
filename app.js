2a1b15f134f50b1d819b240da005be9b3a3ea6d6
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-03-26 11:27:37 -0700

    Completed all exercises in assignment

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Jayson";
+let lastName = "Hundsrucker";
+var age = 18;
 // Exercise 2
-
+lastName = "Hundsrucker";
+age = 18;
 // Exercise 3
+let Language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${Language} was first released in ${createdYear}.`);
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+
+console.log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was on page ${bookmark}.`);
